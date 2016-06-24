bd51e703494122ec0e3878beee4956b7d95388af
Melissa Hubisz <mjhubisz@gmail.com>
2016-06-24 12:42:51 -0400

    small edit to compress boundray case

--- src/argweaver/sequences.h
+++ src/argweaver/sequences.h
@@ -352,7 +352,7 @@ public:
         const int n = all_sites.size();
         for (int pos2 = start; pos2<n; pos2++) {
             if (all_sites[pos2] >= pos) {
-                if (round_dir >= 0 || pos2 == 0) return pos2;
+                if (round_dir >= 0 || pos2 == 0 || pos2 == n - 1) return pos2;
                 else return pos2 - 1;
             }
         }
