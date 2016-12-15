253d0cb8a8d89910643eb09a54d4ec94079ea70e
Melissa Hubisz <mjhubisz@gmail.com>
2016-12-15 15:19:38 -0500

    update meanIterTime to separate leaf/subtree times

--- bin/meanIterTime.sh
+++ bin/meanIterTime.sh
@@ -11,7 +11,19 @@ else
     files=$arg
 fi
 
-grep "sample time" $files |
-    awk '$NF=="s" {totalsec += $(NF-1); count++};
-         $NF=="m" {totalsec += $(NF-1)*60; count++};
-         END {print totalsec/60/count" m"}'
+grep -B 1 "sample time" $files |
+    awk '$0 ~ /resample_arg_regions/ {subtree=1};
+         $0 ~ /resample_arg_leaf/ {subtree=0}
+         $NF=="s" {if (subtree==1) {
+              totalSubtreeSec += $(NF-1); subtreeCount++
+            } else {
+              totalLeafSec += $(NF-1); leafCount++;
+          }}
+         $NF=="m" {if (subtree==1) {
+              totalSubtreeSec += $(NF-1)*60; subtreeCount++;
+            } else {
+              totalLeafSec += $(NF-1)*60; leafCount++;}
+         }
+         END{avgLeaf=totalLeafSec/60/leafCount;
+             avgSubtree=totalSubtreeSec/60/subtreeCount;
+             print "leaf: "avgLeaf" m;  subtree: "avgSubtree" m;"}'
